20845cebf07fd137130d87ffcb076a6114ae921c
JJ208 <46949965+JJ208@users.noreply.github.com>
2022-06-25 16:47:20 -0700

    6/25/22

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 80,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+  
